b4155e293d7b255871dee5781db905be27b75c60
Xiaolong Cao <ATPs@users.noreply.github.com>
2023-06-21 15:18:12 +0800

    Update stability.R

--- R/stability.R
+++ R/stability.R
@@ -1,5 +1,5 @@
 library(reticulate)
-use_python('/share/apps/anaconda3/bin/python3',required = T)
+use_python('/data/p/anaconda3/bin/python3',required = T)
 
 source('./tools.R')
 source('./evaluation.R')
